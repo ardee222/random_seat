c66418432ffe423a7346e62724a02396b8db86b7
ardee222 <ardeeche@gmail.com>
2023-03-03 13:39:40 +0800

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -68,7 +68,7 @@
       </div>
 
       <div class="absolute w-full bottom-0 ">
-        <div class="border text-center ">門口</div>
+        <div class="border text-center text-[#85A894]">門口</div>
       </div>
     </div>
     
@@ -104,7 +104,7 @@
         {/each}
       </div>
       <div class="absolute w-full bottom-0 ">
-        <div name="black_board" class="border text-center"
+        <div name="black_board" class="border text-center text-[#85A894]"
         on:mouseup={change_board}
         class:text-green-300="{clicked === true}">綠板</div>
       </div>
@@ -132,13 +132,13 @@
         {/each}
       </div>
       <div class="absolute w-full bottom-0 ">
-        <div class="border text-center">教壇</div>
+        <div class="border text-center text-[#85A894]">教壇</div>
       </div>
     </div>
   
   </div>
   <div on:mouseup={shuffle} class="flex items-center justify-center space-x-2 mx-auto border max-w-sm w-full font-serif active:bg-white active:text-yellow-500 text-white">
-    <div>抽</div>
+    <div class="text-[#85A894] font-bold">抽</div>
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
       <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 00-3.7-3.7 48.678 48.678 0 00-7.324 0 4.006 4.006 0 00-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3l-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 003.7 3.7 48.656 48.656 0 007.324 0 4.006 4.006 0 003.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3l-3 3" />
     </svg>
